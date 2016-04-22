d4bce3c18f22ef3defdb1b02f611f3ca7832da18
Jon Stutters <j.stutters@ucl.ac.uk>
2016-04-22 16:35:09 +0100

    Set setup to include package data and long description

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
     name='niftianon',
     version='0.1.0',
@@ -8,7 +14,9 @@ setup(
     author='Jon Stutters',
     author_email='j.stutters@ucl.ac.uk',
     description='Anonymiser for NIFTI images',
+    long_description=readme(),
     url='https://github.com/jstutters/niftianon',
+    include_package_data=True,
     install_requires=[
         'Click',
         'nibabel'
